217001f5cc6c2f4dcfafebc98196d73194b20aa8
Travis Liu <xppp3082@arch.nctu.edu.tw>
2024-01-22 22:08:25 +0800

    bug fix修正button無法順利更改display的問題

--- Week-2/Assignment-4/index.js
+++ Week-2/Assignment-4/index.js
@@ -1,6 +1,7 @@
 const btnShowExtra = document.querySelector(".call-button");
+
 btnShowExtra.addEventListener("click", () => {
-  const extraContent = document.querySelector(".extra-container");
+  const extraContent = document.querySelector("#extra-container");
   if (extraContent.style.display === "none") {
     btnShowExtra.textContent = "Hide List";
     extraContent.style.display = "flex";

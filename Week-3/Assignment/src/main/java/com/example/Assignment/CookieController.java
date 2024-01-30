2eefc1e8a4e0f6692e99c13d0191ea80a82d0e5f
Travis Liu <xppp3082@gmail.com>
2024-01-30 15:29:07 +0800

    add Week3 assignment files

---
+++ Week-3/Assignment/src/main/java/com/example/Assignment/CookieController.java
@@ -0,0 +1,53 @@
+package com.example.Assignment;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+
+//@RestController
+@RequestMapping
+@Controller//為了進行頁面渲染，將RestController改成Controller以回傳靜態頁面
+public class CookieController {
+    //不設定default value確定一開始不會找到cookie Name，或設定default為空
+    @GetMapping("/myName")
+    public String myName(@CookieValue(value = "name", defaultValue = "") String userName, Model model) {
+        if (!userName.isEmpty()) {
+            model.addAttribute("message", "Hello, " + userName + "!");
+        } else {
+            model.addAttribute("message", "");
+        }
+        //找不到的話，在後端渲染myName.html template
+        return "myName";
+    }
+
+    @GetMapping("/trackName")
+    public String setCookie(@RequestParam(value = "name", required = false) String userName, HttpServletResponse response){
+        // 創造一個帶有User輸入值的Cookie
+        try{
+            String encodedUserName = Base64.getEncoder().encodeToString(userName.getBytes("UTF-8"));
+            Cookie cookie = new Cookie("name", userName);
+
+            // 設置過期時間，若無設置時間，其生命週期將持續到Session 過期為止
+            cookie.setMaxAge(7*24*60*60);
+            // Add the cookie to the response
+            response.addCookie(cookie);
+
+            //return cookie.getName();
+            //得到cookie後，重新導回/myName;
+            return "redirect:/myName";
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+}
---
+++ Week-3/Assignment/src/main/java/com/example/Assignment/Data.java
@@ -0,0 +1,28 @@
+package com.example.Assignment;
+
+
+import org.springframework.web.bind.annotation.*;
+
+//@RestController
+@RestController
+@RequestMapping()
+public class Data {
+    @GetMapping("data")
+    public String calculate(@RequestParam(value = "number", required = false) String number) {
+        //下次要嘗試將Class分成 Controller、Service、Dao層進行處理
+        int output = 0;
+        if (number == null) {
+            return "Lack of Parameter!";
+        } else {
+            try{
+                int endNum = Integer.parseInt(number);
+                for (int i = 0; i <= endNum; i++) {
+                    output += i;
+                }
+            }catch (NumberFormatException e){
+                return "Wrong Parameter!";
+            }
+        }
+        return String.valueOf(output);
+    }
+}
